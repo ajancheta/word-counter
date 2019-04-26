ab0c05221f630411cd31f68b3119ed05835104f3
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-26 09:08:38 -0700

    add initial code header to files

---
+++ WordCounter.Tests/ModelTests/CounterTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class Counter
+  {
+
+  }
+}
