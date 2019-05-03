5f2d5b698fb4771c5a9d76f15110ffc8f72f131e
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-03 09:07:43 -0700

    add route to game controller

---
+++ WordCounter/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+using System.Collections.Generic;
+
+namespace WordCounter.Controllers
+{
+  public class HomeController : Controller
+  {
+  }
+}
