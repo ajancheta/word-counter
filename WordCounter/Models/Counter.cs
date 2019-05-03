786d63c0e9923d179af0fd7d84410636710ccb92
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-03 15:15:09 -0700

    add functionality to all html pages

--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -5,11 +5,11 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
-    private static string _sentenceInput;
     private static string _wordInput;
+    private static string _sentenceInput;
     private static int _result;
 
-    public RepeatCounter(string sentenceInput, string wordInput, int result)
+    public RepeatCounter(string wordInput, string sentenceInput, int result)
     {
       _sentenceInput = sentenceInput.ToLower();
       _wordInput = wordInput.ToLower();
